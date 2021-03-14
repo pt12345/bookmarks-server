51c8e467667869474fa93962c155d1de81171ac7
pt12345 <ptarara123@gmail.com>
2021-03-14 15:04:36 -0400

    Added POST and DELETE

--- src/bookmarks/bookmark-router.js
+++ src/bookmarks/bookmark-router.js
@@ -1,153 +1,104 @@
 const express = require('express')
-const { bookmarks } = require('../store')
-
-const bookRouter = express.Router()
-const bodyParser = express.json()
-const { v4: uuid } = require('uuid');
+const { isWebUri } = require('valid-url')
+const xss = require('xss')
 const logger = require('../logger')
 const BookmarksService = require('./bookmarks-service')
 
+const bookmarksRouter = express.Router()
+const bodyParser = express.json()
+
 const serializeBookmark = bookmark => ({
   id: bookmark.id,
-  title: bookmark.title,
+  title: xss(bookmark.title),
   url: bookmark.url,
-  description: bookmark.description,
+  description: xss(bookmark.description),
   rating: Number(bookmark.rating),
 })
 
-bookRouter
+bookmarksRouter
   .route('/bookmarks')
   .get((req, res, next) => {
-    const knexInstance = req.app.get('db')
-    BookmarksService.getAllBookmarks(knexInstance)
-    .then(bookmarks => {
-      res.json(bookmarks.map(serializeBookmark))
-    })
-    .catch(next)
+    BookmarksService.getAllBookmarks(req.app.get('db'))
+      .then(bookmarks => {
+        res.json(bookmarks.map(serializeBookmark))
+      })
+      .catch(next)
   })
-  .post(bodyParser, (req, res) => {
-    const { title, url, rating, desc } = req.body;
-
-    console.log(req.body)
-
-    if (!title) {
-        logger.error(`Title is required`);
-        return res
-          .status(400)
-          .send('Invalid data');
-      }
-      
-    if (!url) {
-    logger.error(`URL is required`);
-    return res
-        .status(400)
-        .send('Invalid data');
-    }
-
-    if (!rating) {
-        logger.error(`Rating is required`);
-        return res
-            .status(400)
-            .send('Invalid data');
-    }
-
-    if(rating<0) {
-      logger.error(`Rating cannot be less than zero`);
-      return res
-          .status(400)
-          .send('Invalid data');
-    }
-
-    if (!desc) {
-        logger.error(`Description is required`);
-        return res
-            .status(400)
-            .send('Invalid data');
-    }
-
-    const urlTest = url.split('.');
-
-    console.log(urlTest)
-
-    if(urlTest.length < 3) {
-      logger.error(`Not valid URL`);
-      return res
-          .status(400)
-          .send('Invalid data1');
-    }
-
-    if(urlTest[0] != 'http://www') {
-      if(urlTest[0] != 'https://www') {
-        if(urlTest[0] != 'www') {
-          logger.error(`Not valid URL`);
-          return res
-              .status(400)
-              .send('Invalid data2');
-        }
+  .post(bodyParser, (req, res, next) => {
+    for (const field of ['title', 'url', 'rating']) {
+      if (!req.body[field]) {
+        logger.error(`${field} is required`)
+        return res.status(400).send({
+          error: { message: `'${field}' is required` }
+        })
       }
     }
 
-    if(urlTest[2] != 'com') {
-      logger.error(`Not valid URL`);
-      return res
-          .status(400)
-          .send('Invalid data3');
+    const { title, url, description, rating } = req.body
+
+    const ratingNum = Number(rating)
+
+    if (!Number.isInteger(ratingNum) || ratingNum < 0 || ratingNum > 5) {
+      logger.error(`Invalid rating '${rating}' supplied`)
+      return res.status(400).send({
+        error: { message: `'rating' must be a number between 0 and 5` }
+      })
     }
 
-    const id = uuid();
+    if (!isWebUri(url)) {
+      logger.error(`Invalid url '${url}' supplied`)
+      return res.status(400).send({
+        error: { message: `'url' must be a valid URL` }
+      })
+    }
 
-    const newBookmark = {
-    id,
-    title,
-    url,
-    rating,
-    desc
-    };
+    const newBookmark = { title, url, description, rating }
 
-    bookmarks.push(newBookmark);
-
-    logger.info(`Bookmark with id ${id} created`);
-
-    res
-    .status(201)
-    .location(`http://localhost:8000/bookmarks/${id}`)
-    .json(newBookmark);
+    BookmarksService.insertBookmark(
+      req.app.get('db'),
+      newBookmark
+    )
+      .then(bookmark => {
+        logger.info(`Bookmark with id ${bookmark.id} created.`)
+        res
+          .status(201)
+          .location(`/bookmarks/${bookmark.id}`)
+          .json(serializeBookmark(bookmark))
+      })
+      .catch(next)
   })
 
-bookRouter
-  .route('/bookmarks/:id')
-  .get((req, res, next) => {
-    const { id } = req.params
-    BookmarksService.getById(req.app.get('db'), id)
+bookmarksRouter
+  .route('/bookmarks/:bookmark_id')
+  .all((req, res, next) => {
+    const { bookmark_id } = req.params
+    BookmarksService.getById(req.app.get('db'), bookmark_id)
       .then(bookmark => {
         if (!bookmark) {
-          logger.error(`Bookmark with id ${id} not found.`)
+          logger.error(`Bookmark with id ${bookmark_id} not found.`)
           return res.status(404).json({
             error: { message: `Bookmark Not Found` }
           })
         }
-        res.json(serializeBookmark(bookmark))
+        res.bookmark = bookmark
+        next()
       })
       .catch(next)
   })
-  .delete((req, res) => {
-    const { id } = req.params;
-  
-    const bookIndex = bookmarks.findIndex(li => li.id == id);
-  
-    if (bookIndex === -1) {
-      logger.error(`List with id ${id} not found.`);
-      return res
-        .status(404)
-        .send('Not Found');
-    }
-  
-    bookmarks.splice(bookIndex, 1);
-  
-    logger.info(`Bookmark with id ${id} deleted.`);
-    res
-      .status(204)
-      .end();
+  .get((req, res) => {
+    res.json(serializeBookmark(res.bookmark))
+  })
+  .delete((req, res, next) => {
+    const { bookmark_id } = req.params
+    BookmarksService.deleteBookmark(
+      req.app.get('db'),
+      bookmark_id
+    )
+      .then(numRowsAffected => {
+        logger.info(`Bookmark with id ${bookmark_id} deleted.`)
+        res.status(204).end()
+      })
+      .catch(next)
   })
 
-module.exports = bookRouter
+module.exports = bookmarksRouter
